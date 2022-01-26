b7a2a9f553c9984a7776540a7090dc3db4922f92
harsh160101065 <harshmittal160101065@gmail.com>
2022-01-26 20:00:09 +0530

    Added new features

---
+++ script.js
@@ -0,0 +1,26 @@
+var fn = prompt("First Number ")
+var operation = prompt("Enter an operation ")
+switch(operation)
+{ 
+    case "/":
+		var seca = prompt("Secound Number")
+        document.write(fn/sec1)
+        break;
+	case "*":
+		var sec2 = prompt("Secound Number")
+        document.write(fn*sec2)
+        break;
+		
+
+	case "+":
+		var sec3 = prompt("Secound Number")
+        document.write(fn+sec3);
+        break;
+		
+
+    case "-":
+		var sec4 = prompt("Secound Number")
+        document.write(fn-sec4)	
+        break;       		
+
+}
